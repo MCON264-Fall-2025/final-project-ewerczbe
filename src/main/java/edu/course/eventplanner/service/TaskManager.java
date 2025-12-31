d58c1816463c7333dc3c0f16b82132e559ce4bed
Etty Werczberger <ewerczbe@student.touro.edu>
2025-12-31 16:54:50 +0200

    created the logic for the classes and implemented the main with a menu console

--- src/main/java/edu/course/eventplanner/service/TaskManager.java
+++ src/main/java/edu/course/eventplanner/service/TaskManager.java
@@ -4,10 +4,35 @@ import edu.course.eventplanner.model.Task;
 import java.util.*;
 
 public class TaskManager {
+
     private final Queue<Task> upcoming = new LinkedList<>();
     private final Stack<Task> completed = new Stack<>();
-    public void addTask(Task task) { /* TODO */ }
-    public Task executeNextTask() { return null; }
-    public Task undoLastTask() { return null; }
-    public int remainingTaskCount() { return upcoming.size(); }
+
+
+    public void addTask(Task task) {
+        if (task != null) {
+            upcoming.add(task);
+        }
+    }
+    public Task executeNextTask() {
+        if (upcoming.isEmpty()) {
+            return null;
+        }
+
+        Task next = upcoming.poll();
+        completed.push(next);
+        return next;
+    }
+
+    public Task undoLastTask() {
+        if (completed.isEmpty()) {
+            return null;
+        }
+
+        return completed.pop();
+    }
+
+    public int remainingTaskCount() {
+        return upcoming.size();
+    }
 }
